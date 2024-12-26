28caa34eca17c63451707c07c64d4bf4290eb538
iskrot <olga.volwskaya@gmail.com>
2024-12-26 12:33:23 +0300

    Внеса поправки в задание №6

--- src/Main.java
+++ src/Main.java
@@ -33,13 +33,17 @@ public class Main {
         float money = 0;
         for (byte i = 1; i <= 12; i++) {
             money += 29000;
-            System.out.println(" Месяц "+i+", сумма накоплений равна " + money + " рублей");
+            System.out.println(" Месяц " + i + ", сумма накоплений равна " + money + " рублей");
         }
 
         for (byte i = 1; i <= 12; i++) {
             money *= 1.12;
             money += 29000;
-            System.out.println(" Месяц "+i+", сумма накоплений равна " + money + " рублей");
+            System.out.println(" Месяц " + i + ", сумма накоплений равна " + money + " рублей");
+        }
+
+        for (byte i = 1; i <= 10; i++){
+            System.out.println(2*i);
         }
 
     }
