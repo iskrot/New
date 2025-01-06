4b7557ee4804d185ccf67c4989749e93afcf13ad
iskrot <olga.volwskaya@gmail.com>
2025-01-07 00:17:53 +0300

    сделала задание №8

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper =763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog = dog+ 4;
-        cat = cat+ 4;
-        paper += 4;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog = dog-3.5;
-        cat = cat-1.6;
-        paper -= 7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        var friend = 19;
-        System.out.println(friend);
-        friend += 2;
-        System.out.println(friend);
-        friend /= 7;
-        System.out.println(friend);
-        var frog = 3.5;
-        System.out.println(frog);
-        frog *= 10;
-        System.out.println(frog);
-        frog += 4;
-        System.out.println(frog);
-        var men1 = 78.2;
-        var men2 = 82.7;
-        System.out.println(men1 + men2);
-        System.out.println(men2 - men1);
-        System.out.println((men1 + men2) % (men2 - men1));
-        var time = 640;
-        var timework = 8;
-        var workers = time / timework;
-        System.out.println("Всего работников в компании — " +workers + " человек.");
-        workers += 94;
-        time = timework * workers;
-        System.out.println( "Если в компании работает " +workers + " человек, то всего " +time + " часов работы может быть поделено между сотрудниками.");
+        int[] a = new int[3];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        float[] b = {1.57f, 7.654f, 9.986f};
+        byte[] c = {1, 2, 3};
 
+        System.out.print(a[0]);
+        for (byte i = 1; i <= 2;i++ ){
+            System.out.print(","+a[i]);
+        }
+        System.out.println();
+        System.out.print(b[0]);
+        for (byte i = 1; i <= 2;i++ ){
+            System.out.print(","+b[i]);
+        }
+        System.out.println();
+        System.out.print(c[0]);
+        for (byte i = 0; i <= 2;i++ ){
+            System.out.print(","+c[i]);
+        }
+        System.out.println();
+
+        System.out.print(a[2]);
+        for (byte i = 1; i <= 2;i++ ){
+            System.out.print(","+a[a.length-i-1]);
+        }
+        System.out.println();
+        System.out.print(b[2]);
+        for (byte i = 0; i <= 2;i++ ){
+            System.out.print(","+b[b.length-i-1]);
+        }
+        System.out.println();
+        System.out.print(c[2]);
+        for (byte i = 0; i <= 2;i++ ){
+            System.out.print(","+c[c.length-i-1]);
+        }
+        System.out.println();
+
+        for (byte i = 0; i < a.length; i++){
+            if (a[i]%2!=0){
+                a[i] += 1;
+            }
+        }
+        System.out.print(a[0]);
+        for (byte i = 1; i <= 2;i++ ){
+            System.out.print(","+a[i]);
+        }
     }
 }
