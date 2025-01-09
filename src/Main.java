259d37576a3b2ad70214d4a51f50db5a53ef4ac2
iskrot <olga.volwskaya@gmail.com>
2025-01-09 16:19:56 +0300

    сделала задание №9

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper =763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog = dog+ 4;
-        cat = cat+ 4;
-        paper += 4;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog = dog-3.5;
-        cat = cat-1.6;
-        paper -= 7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        var friend = 19;
-        System.out.println(friend);
-        friend += 2;
-        System.out.println(friend);
-        friend /= 7;
-        System.out.println(friend);
-        var frog = 3.5;
-        System.out.println(frog);
-        frog *= 10;
-        System.out.println(frog);
-        frog += 4;
-        System.out.println(frog);
-        var men1 = 78.2;
-        var men2 = 82.7;
-        System.out.println(men1 + men2);
-        System.out.println(men2 - men1);
-        System.out.println((men1 + men2) % (men2 - men1));
-        var time = 640;
-        var timework = 8;
-        var workers = time / timework;
-        System.out.println("Всего работников в компании — " +workers + " человек.");
-        workers += 94;
-        time = timework * workers;
-        System.out.println( "Если в компании работает " +workers + " человек, то всего " +time + " часов работы может быть поделено между сотрудниками.");
+        String firstName = "";
+        String middleName = "";
+        String lastName = "";
+        String fullName = "Ivanov Ivan Ivanovich";
+        byte i = 0;
+        while (i < fullName.length()) {
+            if (fullName.charAt(i) != ' ') {
+                lastName = lastName + fullName.charAt(i);
+                i++;
+            } else {
+                i++;
+                break;
+            }
+        }
+        while (i < fullName.length()) {
+            if (fullName.charAt(i) != ' ') {
+                firstName = firstName + fullName.charAt(i);
+                i++;
+            } else {
+                i++;
+                break;
+            }
+        }
+        while (i < fullName.length()) {
+            if (fullName.charAt(i) != ' ') {
+                middleName = middleName + fullName.charAt(i);
+                i++;
+            }
+        }
+        System.out.println("Ф. И. О. сотрудника — " + firstName + " " + middleName + " " + lastName);
 
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName);
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
